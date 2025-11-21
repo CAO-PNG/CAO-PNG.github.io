3aeae5f31422bb6c1836337b048755761cea99f6
CAO-PNG <918360242@qq.com>
2025-11-21 16:44:19 +0800

    Site updated: 2025-11-21 16:44:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/21/writeup/2025-11-21-CISCN_2021_初赛_silverwolf/"];function toRandomPost(){
+var posts=[];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
